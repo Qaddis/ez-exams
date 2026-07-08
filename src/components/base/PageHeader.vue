fb684984321a82033140d2a9a89556573f6d0eb6
Qaddis <svyatoslavb1107@gmail.com>
2026-07-08 11:28:57 +0300

    feat: add PageHeader base component

---
+++ src/components/base/PageHeader.vue
@@ -0,0 +1,28 @@
+<template>
+	<h2 class="heading">
+		<slot />
+	</h2>
+</template>
+
+<style scoped>
+.heading {
+	width: 100%;
+	background-color: var(--light-color);
+	border-bottom: 2px solid var(--secondary-color);
+	padding-bottom: 5px;
+	margin-bottom: 15px;
+
+	font-size: 1.8rem;
+	font-family: var(--comic-font);
+	font-weight: bold;
+	color: var(--primary-color);
+	text-align: center;
+
+	position: sticky;
+	left: 0;
+	top: 0;
+	z-index: 10;
+
+	box-shadow: 0 6.5px 4px -5px rgba(0, 0, 0, 0.45);
+}
+</style>
