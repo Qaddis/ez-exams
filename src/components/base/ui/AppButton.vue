e916c4375679f3c07624d204281cfb71aebbca35
Qaddis <svyatoslavb1107@gmail.com>
2026-07-14 13:38:20 +0300

    feat: add AppButton ui component

---
+++ src/components/base/ui/AppButton.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import type { ButtonHTMLAttributes } from "vue"
+
+interface IProps extends /* @vue-ignore */ ButtonHTMLAttributes {
+	variant?: "default" | "outlined"
+}
+
+withDefaults(defineProps<IProps>(), {
+	variant: "default"
+})
+</script>
+
+<template>
+	<button class="button" :class="[variant]">
+		<slot />
+	</button>
+</template>
+
+<style scoped>
+.button {
+	border: 1px solid var(--primary-color);
+	border-radius: 5px;
+	width: 100%;
+	padding: 3.5px 12px;
+
+	font-size: 1.05rem;
+
+	transition:
+		opacity 0.15s,
+		translate 0.15s,
+		color 0.25s,
+		border-color 0.25s,
+		background-color 0.25s;
+}
+
+.button:active {
+	opacity: 0.85;
+	translate: 0 2px;
+}
+
+.button:focus-visible {
+	outline: 2px dashed var(--accent-color);
+	outline-offset: 2px;
+}
+
+.default {
+	background: var(--primary-color);
+	color: var(--light-color);
+}
+
+.default:hover {
+	background-color: var(--accent-color);
+	border-color: var(--accent-color);
+}
+
+.outlined:hover {
+	background-color: var(--primary-color);
+	color: var(--light-color);
+}
+</style>
