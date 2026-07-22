f24bf5d6fd3401a0812f2a69ce9368551f4d35a3
Qaddis <svyatoslavb1107@gmail.com>
2026-07-22 17:40:31 +0300

    refactor: add documentation for format dates util

--- src/utils/formatDates.ts
+++ src/utils/formatDates.ts
@@ -1,3 +1,8 @@
+/**
+ * Форматирует дату формата ISO-строки в корректную для отображения в локальном часовом поясе
+ * @param date дата в формате ISO-строки
+ * @returns Дата для вывода в локальном часовом поясе
+ */
 export const getLocaleDate = (date: string): string => {
 	const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone
 
